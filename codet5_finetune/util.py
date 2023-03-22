9116c3c8be12d8259254f9335dd193c2ef9f4440
Denis Kocetkov <denis.kocetkov@servicenow.com>
2023-03-22 15:06:35 +0000

    added generate stop criteria by a set of token ids

--- codet5_finetune/util.py
+++ codet5_finetune/util.py
@@ -2,9 +2,25 @@ import random
 import numpy as np
 import os
 
+import torch
+from transformers import StoppingCriteria 
+
 def set_global_seeds(opt):
     # TODO: add per rank seed mofification
     #       and add rank setting in options
     np.random.seed(opt.seed)
     os.environ['PYTHONHASHSEED'] = str(opt.seed)
     random.seed(opt.seed)
+
+class StoppingCriteriaTokenIds(StoppingCriteria):
+    def __init__(self, stop_ids, device='cuda'):
+        super().__init__()
+        self.stop_ids = torch.tensor(stop_ids).to(device)
+
+    def __call__(self, input_ids, scores, *kwargs):
+        # true if any alement is one of the stop ids in all rows
+        return torch.all(torch.any(
+            torch.isin(input_ids, self.stop_ids),
+            dim=1,
+            keepdim=False
+        )).item()
