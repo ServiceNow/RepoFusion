56a51305bb13b0efb300bbbc927fe95b7954fa9b
Denis Kocetkov <denis.kocetkov@servicenow.com>
2023-03-29 11:52:44 +0000

    added save inputs on excetion and fix to remove -100 form inputs

--- codet5_finetune/metrics.py
+++ codet5_finetune/metrics.py
@@ -30,54 +30,67 @@ def exact_matches_ratio(a, b, include_empty_matches):
     return sum(comp(el_a, el_b) for el_a, el_b in zip(a, b)) / len(a)
 
 def compute_metrics(preds, ctx):
-    label_ids = np.where(preds.label_ids != -100, preds.label_ids, ctx.tokenizer.pad_token_id)
-    labels = ctx.tokenizer.batch_decode(label_ids, skip_special_tokens=True)
-    predictions = ctx.tokenizer.batch_decode(preds.predictions, skip_special_tokens=True)
-    inputs = ctx.tokenizer.batch_decode(preds.inputs)
-    # if opt.strip_new_lines_for_em:
-    #     def strip_get_first_line(items):
-    #         items = [el.splitlines() for el in items]
-    #         items_first_line = [el[0] for el in items]
-    #         items = [''.join(el) for el in items]
-    #         return items, items_first_line
-    #     labels, labels_first_line = strip_get_first_line(labels)
-    #     predictions, predictions_first_line = strip_get_first_line(predictions)
-    # else:
-    def get_first_lines(vals):
-        return [
-            lines[0] if len(lines) > 0 else ''
-            for lines in (
-                el.splitlines()
-                for el in vals
-            )
-        ]
-    labels_first_line = get_first_lines(labels)
-    predictions_first_line = get_first_lines(predictions)
+    try:
+        label_ids = np.where(preds.label_ids != -100, preds.label_ids, ctx.tokenizer.pad_token_id)
+        labels = ctx.tokenizer.batch_decode(label_ids, skip_special_tokens=True)
 
-    # TODO: get several exactly matched and several not matched samples instead
-    # for now save just 500 of first examples
-    sz = min(500, len(inputs))
-    examples = {
-        'full': [
-            {'input': input, 'label': label, 'prediction': prediction}
-            for input, label, prediction in zip(inputs[:sz], labels[:sz], predictions[:sz])
-        ],
-        'first_line': [
+        # TODO: seems trainer pads all seq with -100 then aggregates different batches for compute_metrics
+        #       or in some other place, check this and find if this can be changes instead of replacing here
+        predictions = np.where(preds.label_ids != -100, preds.predictions, ctx.tokenizer.pad_token_id)
+        inputs = np.where(preds.label_ids != -100, preds.inputss, ctx.tokenizer.pad_token_id)
+
+        predictions = ctx.tokenizer.batch_decode(predictions, skip_special_tokens=True)
+        inputs = ctx.tokenizer.batch_decode(inputs)
+        # if opt.strip_new_lines_for_em:
+        #     def strip_get_first_line(items):
+        #         items = [el.splitlines() for el in items]
+        #         items_first_line = [el[0] for el in items]
+        #         items = [''.join(el) for el in items]
+        #         return items, items_first_line
+        #     labels, labels_first_line = strip_get_first_line(labels)
+        #     predictions, predictions_first_line = strip_get_first_line(predictions)
+        # else:
+        def get_first_lines(vals):
+            return [
+                lines[0] if len(lines) > 0 else ''
+                for lines in (
+                    el.splitlines()
+                    for el in vals
+                )
+            ]
+        labels_first_line = get_first_lines(labels)
+        predictions_first_line = get_first_lines(predictions)
+
+        # TODO: get several exactly matched and several not matched samples instead
+        # for now save just 500 of first examples
+        sz = min(500, len(inputs))
+        examples = {
+            'full': [
                 {'input': input, 'label': label, 'prediction': prediction}
-            for input, label, prediction in zip(inputs[:sz], labels_first_line[:sz], predictions_first_line[:sz])
-        ]
-    }
-    if ctx.opt.is_main:
-        pid = os.getpid()
-        step = len(list(ctx.examples_dir.glob('*.json')))
-        example_file = ctx.examples_dir / f'{step}_{pid}.json'
-        with example_file.open('wt') as f:
-            json.dump(examples, f)
+                for input, label, prediction in zip(inputs[:sz], labels[:sz], predictions[:sz])
+            ],
+            'first_line': [
+                    {'input': input, 'label': label, 'prediction': prediction}
+                for input, label, prediction in zip(inputs[:sz], labels_first_line[:sz], predictions_first_line[:sz])
+            ]
+        }
+        if ctx.opt.is_main:
+            pid = os.getpid()
+            step = len(list(ctx.examples_dir.glob('*.json')))
+            example_file = ctx.examples_dir / f'{step}_{pid}.json'
+            with example_file.open('wt') as f:
+                json.dump(examples, f)
 
-    return {
-        #'em_ratio': exact_matches_ratio(predictions, labels),
-        #'em_a2p_ratio': average_exact_match_a2p_ratio(predictions, labels),
-        'em_first_line_ratio': exact_matches_ratio(labels_first_line, predictions_first_line, include_empty_matches=True),
-        'em_first_line_ratio_wo_empty_matches': exact_matches_ratio(labels_first_line, predictions_first_line, include_empty_matches=False),
-        #'examples': str(examples)
-    }
+        return {
+            #'em_ratio': exact_matches_ratio(predictions, labels),
+            #'em_a2p_ratio': average_exact_match_a2p_ratio(predictions, labels),
+            'em_first_line_ratio': exact_matches_ratio(labels_first_line, predictions_first_line, include_empty_matches=True),
+            'em_first_line_ratio_wo_empty_matches': exact_matches_ratio(labels_first_line, predictions_first_line, include_empty_matches=False),
+            #'examples': str(examples)
+        }
+    except Exception:
+        if ctx.opt.is_main:
+            np.save(ctx.examples_dir / 'labels.npy', preds.label_ids)
+            np.save(ctx.examples_dir / 'predictions.npy', preds.predictions)
+            np.save(ctx.examples_dir / 'inputs.npy', preds.inputs)
+        raise 
